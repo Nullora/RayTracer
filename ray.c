2b9918ce2df8d862c896bbfad8719990f8b350dc
Nullora <nullora@tutamail.com>
2026-04-01 22:47:43 +0100

    its slow as shit

--- ray.c
+++ ray.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<SDL2/SDL.h>
-//forget the file parsing i wanna raytrace
+//fuck the file parsing i wanna trace rays
 
 int main(){
     const int width=900;
@@ -9,12 +9,20 @@ int main(){
     SDL_Window *pwindow = SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
     Uint8 r,g,b;
-    r = 0xff;
+    r = 0x00;
     g = 0xff;
     b = 0x00;
-    Uint32 color = SDL_MapRGB(psurface->format, r,g,b);
+    Uint32 color = 0x00;
     SDL_FillRect(psurface,NULL,color);
     SDL_UpdateWindowSurface(pwindow);
+
+    for(int i = 0; i<256;i++){
+        r++;
+        color = SDL_MapRGB(psurface->format, r,g,b);
+        SDL_FillRect(psurface,NULL,color);
+        SDL_UpdateWindowSurface(pwindow);
+        SDL_Delay(100);
+    }
     int running = 1;
     while(running){
         SDL_Event e;
